b279814a8e97e186b23f61667583a70ab6732afd
BLXCKBXXST <blxckbusta@bk.ru>
2026-05-16 00:13:12 +0300

    refactor(lab-12,13,14): unify around sneakers track, drop dual-variant layout

--- lab-13/katysha-roguelike/grade-5/pipes/read_pipe.c
+++
@@ -1,2 +0,0 @@
-#include "pipes.h"
-void read_pipe(int p,char *b,int size){while(size){int r=read(p,b,size);if(r<=0)exit(0);b+=r;size-=r;}}
--- lab-13/katysha-roguelike/grade-5/src/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "background.h"
-int main(void){
-    start_background_system();
-    background_system_msg("echo Roguelike room loaded!");
-    return 0;
-}
